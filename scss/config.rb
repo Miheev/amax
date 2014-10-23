8eb957146dcbc1666a4b5d905486ca31e0d62a9a
Miheev Sergey <serega.khv@gmail.com>
2014-10-23 18:58:16 +1100

    html init

--- scss/config.rb
+++ scss/config.rb
@@ -3,35 +3,40 @@
 # quickly dive right in.
 # For more info about compass + SASS: http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
 
+environment = :production
 
 #########
 # 1. Set this to the root of your project when deployed:
 http_path = "/"
 
-# 2. probably don't need to touch these
+# Location of the theme's resources.
 css_dir = "../css"
 sass_dir = "./"
 images_dir = "../img"
+generated_images_dir = images_dir + "/generated"
 javascripts_dir = "../js"
-environment = :production
-relative_assets = true
-
-
-# 3. You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
-
-# 4. When you are ready to launch your WP theme comment out (3) and uncomment the line below
-# output_style = :compressed
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# don't touch this
-preferred_syntax = :scss
 
 # Require any additional compass plugins installed on your system.
-#require 'compass-normalize'
+require 'compass-normalize'
 require 'rgbapng'
 require 'toolkit'
+require 'breakpoint'
+require 'singularitygs'
 require 'susy'
 require 'sass-globbing'
+
+
+# You can select your preferred output style here (:expanded, :nested, :compact
+# or :compressed).
+output_style = (environment == :production) ? :expanded : :nested
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from the server omega.
+relative_assets = true
+
+# Conditionally enable line comments when in development mode.
+line_comments = (environment == :production) ? false : true
+
+# Output source maps in development mode.
+sass_options = (environment == :production) ? {} : {:sourcemap => true}
