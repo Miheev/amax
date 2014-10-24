4a34f0b89085a6f7399c40354260068162625505
Miheev Sergey <serega.khv@gmail.com>
2014-10-24 20:14:57 +1100

    Header, Footer, Catalog, Pre Cat-Menu

--- js/main.js
+++ js/main.js
@@ -6,6 +6,28 @@ var media_table_landscape= 1000;
 
 //(function ($) {
 $(document).ready(function() {
-    //$('ul.sf-menu').superfish();
+    $('ul.sf-menu').superfish();
+
+    $('.select .abbr').click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        if ($(this).next().css('display') == 'none') {
+            $(this).next().css('display', 'block');
+            //$(this).find('.pointer').css('background-position', ' -16px 2px');
+        } else {
+            $(this).next().css('display', 'none');
+            //$(this).find('.pointer').css('background-position', ' 0 2px');
+        }
+    });
+    $('.select .list a').click(function(e){
+        e.preventDefault();
+        $(this).parents('.select').find('.label').text($(this).text().trim());
+    });
+    $('body').click(function(){
+        $('.select .list').css('display', 'none');
+        //$('.select .pointer').css('background-position', ' 0 2px');
+    });
+
 }); /* end of as page load scripts */
 //})(jQuery);
